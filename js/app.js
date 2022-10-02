c494857e79660c405f3bc955dead386ba51de34e
yannick2009 <yannickkouakou13@gmail.com>
2022-10-02 08:30:39 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const drumButton = document.querySelectorAll(".drumpad-btn button");
+const screen = document.querySelector(".screen");
+
+drumButton.forEach(async (e) => {
+  await e.addEventListener("click", () => {
+    const song = new Audio(`../audio/drum/${e.dataset.nom}.WAV`);
+    song.play();
+    screen.textContent = `${e.dataset.nom}`;
+  });
+});
+
+document.addEventListener("keydown", async (e) => {
+  await drumButton[Number(e.key) - 1].click();
+  drumButton[Number(e.key) - 1].classList.add("active");
+});
+
+document.addEventListener("keyup", async (e) => {
+  drumButton[Number(e.key) - 1].classList.remove("active");
+});
